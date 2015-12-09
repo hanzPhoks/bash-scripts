261224dd66eca4fdc162aba9cdc1aef88a81bc82
Jean Arnoldy <jar@CESNCFSTRA.local>
2015-12-09 18:27:22 +0100

    ajout support mysql

--- backup.sh
+++ backup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# when = everyday at 02
+#title           :backup.sh
+#description     :This is a bash backup script for folders and Databases whith history.
+#author          :HanzPhoks 
+#date            :2015-12-09
+#version         :0.2
+#usage           :bash backup.sh
+#notes           :Change the settings in the "Parameters" section as you need.
+
 # for backupninja
+# when = everyday at 02
 
 set -e
 startedAt=$(date +%s)
@@ -10,6 +18,20 @@ startedAt=$(date +%s)
 # ------------------------------------
 # list here the folders to backup
 foldersToBackup=("/var/www/" "/var/backups/postgres/")
+
+# postgresql only suports localhost witohut password, if you have a solution, please tell me
+dbType="postgresql" #mysql or postgresql 
+dbHost="localhost"
+databases="all" # all or databases names in array like: databases=("dbname1" "dbname2" "dbname3")
+exludedDatabases="(Database|information_schema|test)" # useles databases to skip
+backupEachDBTable="yes" # if set to yes, it will create a backup file per db table, else it will create a file per database
+dbRoot="root" # root or <username> whith sufficient rights
+
+$passwdFile="/root/.p" # you can change the file, this script will automatically change the rights to 400
+dbPasswd=$(cat $passwdFile)
+chown root $passwdFile
+chmod 100 $passwdFile
+
 localBackupFolder="/var/backups/history"
 remoteBackupFolder="/mnt/nas/history"
 logFile="/var/log/backup.log"
@@ -17,6 +39,7 @@ debugLogFile="/var/log/backup-debug.log"
 errorLogFile="/var/log/backup-error.log"
 keepLocalBackups=10
 keepRemoteBackups=10
+
 emailTo="jar@cesncf-stra.org"
 emailLogs="always"
 # debug always error
@@ -30,16 +53,10 @@ backupFolderFullName="$localBackupFolder/$backupFolderName"
 #  End of parameters
 # ------------------------------------
 
-
-# ------------------------------------
-# Script
-# ------------------------------------
-
-# Fichier log
+# Fichiers log
 if [ -s $logFile ]; then rm $logFile; else touch $logFile; fi
 if [ -s $debugLogFile ]; then rm $debugLogFile; else touch $debugLogFile; fi
 if [ -s $errorLogFile ]; then rm $errorLogFile; else touch $errorLogFile; fi
-
 exec >> $debugLogFile
 exec 2>> $errorLogFile
 
@@ -47,12 +64,64 @@ exec 2>> $errorLogFile
 if [ ! -s $localBackupFolder ];then mkdir $localBackupFolder;fi
 if [ ! -s $remoteBackupFolder ];then mkdir $remoteBackupFolder;fi
 
-echo "Starting backup of ${foldersToBackup[@]}" > $logFile
+echo "Starting backup of $dbType databases" > $logFile
+#db backup
+case $dbType in
+    "mysql")
+            mkdir "/tmp/mysql"
+            foldersToBackup+=("/tmp/mysql/")
+            if [ $databases = "all" ] ;then 
+                    databases=$(mysql -u $dbRoot -p$dbPasswd -h $dbHost -e "show databases;" | tr -d "|" | grep -Ev "$exludedDatabases")
+            fi
+            for db in $databases; do
+                    if [ $backupEachDBTable = "yes" ]; then
+                            tables=$(mysql -u $dbRoot -p$dbPasswd -h $dbHost $db -e "show tables;" | tr -d "|")
+                            for table in $tables; do 
+                                    mysqldump --opt -u $dbRoot -p$dbPasswd -h $dbHost $db $table > /tmp/mysql/${db}/${table}.sql
+                            done
+                    else
+                            mysqldump --opt -u $dbRoot -p$dbPasswd -h $dbHost $db > /tmp/mysql/${db}.sql
+                    fi
+            done
+            ;;
+    "postgresql")
+        mkdir "/tmp/postgresql"
+        foldersToBackup+=("/tmp/postgresql/")
+        if [ $databases = "all" ] ;then
+            databases=$(psql -At -c "select datname from pg_database where not datistemplate and datallowconn;" postgres $dbRoot)
+        fi
 
+        for db in $databases
+        do
+            if 
+            if ! pg_dump -Fp "$db" | gzip > $dossier_sauvegarde"$db".sql.gz.in_progress; then
+                echo $date" --> !! Sauvegarde de $db incomplète, voir $dossier_sauvegarde"$db".sql.gz.in_progress" >> $fichier_log
+            else
+                mv $dossier_sauvegarde"$db".sql.gz.in_progress $dossier_sauvegarde"$db".sql.gz
+                echo $date" --> Sauvegarde de la base $db complète, voir $dossier_sauvegarde"$db".sql.gz" >> $fichier_log
+            fi
+        done
+        for db in $databases; do
+            if [ $backupEachDBTable = "yes" ]; then
+                tables=$(mysql -u $dbRoot -p$dbPasswd -h $dbHost $db -e "show tables;" | tr -d "|")
+                for table in $tables; do
+                    mysqldump --opt -u $dbRoot -p$dbPasswd -h $dbHost $db $table > /tmp/mysql/${db}/${table}.sql
+                done
+            else
+                    mysqldump --opt -u $dbRoot -p$dbPasswd -h $dbHost $db > /tmp/mysql/${db}.sql
+            fi
+        done
+        ;;
+    *)
+        echo "Error: unsuported database type. Please change the dbType parameter." >> $errorLogFile
+        ;;
+esac
+
+echo "Starting backup of ${foldersToBackup[@]}" > $logFile
 # copie des dossiers a sauvegarder
 for folder in "${foldersToBackup[@]}"; do
-        echo "Copying $folder " >> $logFile
-        cp -v -R -- $folder $backupFolderFullName
+    echo "Copying $folder " >> $logFile
+    cp -v -R -- $folder $backupFolderFullName
 done
 
 # Compressing, copy to to remote and remove
@@ -77,23 +146,23 @@ endedAt=$(date +%s)
 executionTime=$(echo $endedAt-$startedAt | bc )
 
 if [  $executionTime -gt 60 ]; then
-        minutes=$(echo $executionTime / 60 | bc)
-        seconds=$(echo $executionTime % 60 | bc)
-        echo "The script took $minutes minutes and $seconds seconds " >> $logFile
-        echo "The script took $minutes minutes and $seconds seconds " >> $debugLogFile
+    minutes=$(echo $executionTime / 60 | bc)
+    seconds=$(echo $executionTime % 60 | bc)
+    echo "The script took $minutes minutes and $seconds seconds " >> $logFile
+    echo "The script took $minutes minutes and $seconds seconds " >> $debugLogFile
 else
-        echo "The script took $executionTime seconds " >> $logFile
-        echo "The script took $executionTime seconds " >> $debugLogFile
+    echo "The script took $executionTime seconds " >> $logFile
+    echo "The script took $executionTime seconds " >> $debugLogFile
 fi
 
 case $emailLogs in
-"debug")
+    "debug")
         cat $errorLogFile $debugLogFile | mail -s "Backup debug result from $(hostname -f)" $emailTo;;
-"always")
+    "always")
         cat $logFile | mail -s "Backup result from $(hostname -f)" $emailTo;;
-"error")
+    "error")
         if [ -s $errorLogFile ]; then cat $errorLogFile | mail -s "An error occured while backup at $(hostname -f)" $emailTo; fi;;
-*)
+    *)
         echo "You may have a configuration error in the backup script parameters. Please check the emailLog parameter" | mail -s "Backup for srvwan error log" $emailTo;;
 esac
 
